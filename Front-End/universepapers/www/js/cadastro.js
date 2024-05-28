486f57fd477ab1326710bbd38b631dfac2a69e2d
vitormuniz0 <vitormuniz311@gmail.com>
2024-05-28 20:23:44 -0300

    aaa

--- Front-End/universepapers/www/js/cadastro.js
+++ Front-End/universepapers/www/js/cadastro.js
@@ -1,13 +1,26 @@
-document.getElementById('cadastroForm').addEventListener('submit', function(event) {
+import { api } from "../services";
+
+document.getElementById('cadastroForm').addEventListener('submit', async function (event) {
     event.preventDefault();
     const nome = document.getElementById('nome').value;
     const email = document.getElementById('email').value;
-    const usuario = document.getElementById('usuario').value;
     const senha = document.getElementById('senha').value;
 
-    if (nome && email && usuario && senha) {
-        alert('Cadastro realizado com sucesso!');
-    } else {
-        alert('Por favor, preencha todos os campos.');
+    const data = {
+        nome, email, senha
+    }
+
+    try {
+        const response = await api.post("/createUser", data);
+        console.log(response.data)
+        alert("Usuário Criado com Sucesso!");
+    } catch (error) {
+        if (error.response && error.response.status === 400) {
+            setError("Usuário já existe. Por favor, escolha um email diferente.");
+            console.log(error)
+            alert("Usuário já Existe!")
+        } else {
+            setError("Ocorreu um erro ao processar sua solicitação. Por favor, tente novamente mais tarde.");
+        }
     }
 });
